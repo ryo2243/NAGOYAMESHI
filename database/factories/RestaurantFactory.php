1869594db7a7cde7d0a29f02ce49aba4309fef49
Ryo Saito <rsaito1620@gmail.com>
2022-09-19 12:13:29 +0900

    feat: 本番環境向けにSeederの画像を追加

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -21,7 +21,7 @@ class RestaurantFactory extends Factory {
 
         return [
             'name' => $this->faker->name,
-            'image' => basename(asset('/images/dummy.jpg')),
+            'image' => basename('public/dummy.jpg'),
             'description' => $this->faker->realText,
             'lowest_price' => $dummy_lowest_price,
             'highest_price' => $dummy_lowest_price + 2000,
