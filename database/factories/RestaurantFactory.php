880204ae3295c439a0c4c888ba33e581b6a06792
Ryo Saito <rsaito1620@gmail.com>
2022-09-19 11:52:15 +0900

    fix: 本番環境向けにSeederの画像パスを修正

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -21,7 +21,7 @@ class RestaurantFactory extends Factory {
 
         return [
             'name' => $this->faker->name,
-            'image' => basename('public/images/dummy.jpg'),
+            'image' => basename(asset('/images/dummy.jpg')),
             'description' => $this->faker->realText,
             'lowest_price' => $dummy_lowest_price,
             'highest_price' => $dummy_lowest_price + 2000,
