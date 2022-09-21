4c9a07f46d99d235b29166ddaf95ffa9c981ef0d
Ryo Saito <rsaito1620@gmail.com>
2022-09-22 06:32:06 +0900

    feat: 管理者画面のトップページを作成

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 
 class HomeController extends Controller {
     /**
@@ -21,6 +25,28 @@ class HomeController extends Controller {
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index() {
-        return view('admin.home');
+        $total_users = User::count();
+        $total_subscribers = DB::table('subscriptions')->where('stripe_status', 'active')->count();
+        $total_non_subscribers = $total_users - $total_subscribers;
+        $total_restaurants = Restaurant::count();
+        $total_reservations = Reservation::count();
+
+        $price = 300;
+
+        $subscription_quantity = DB::table('subscriptions')
+            ->whereRaw('ends_at >=' . date('Y-m-', strtotime('now')) . date('d', strtotime('created_at')))
+            ->orWhere('stripe_status', 'active')->count();
+        $sales_for_this_month = 300 * $subscription_quantity;
+
+        $variables = [
+            'total_users',
+            'total_subscribers',
+            'total_non_subscribers',
+            'total_restaurants',
+            'total_reservations',
+            'sales_for_this_month'
+        ];
+
+        return view('admin.home', compact($variables));
     }
 }
